4c1d5d0d14e8c2592f73693e3d58f18f10d03d21
zhaoxin1998 <1778524294@qq.com>
2023-07-18 16:24:49 +0800

    增加插件

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,12 @@
 import { defineUserConfig } from "vuepress";
-import { hopeTheme } from "vuepress-theme-hope";
-// import { searchProPlugin } from 'vuepress-plugin-search-pro';
 
-// import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-
+import { commentPlugin } from "vuepress-plugin-comment2";
+import { copyrightPlugin } from "vuepress-plugin-copyright2";
+import { readingTimePlugin } from "vuepress-plugin-reading-time2";
+import { lightgalleryPlugin } from "vuepress-plugin-lightgallery";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
+import { nprogressPlugin } from "@vuepress/plugin-nprogress";
+import { prismjsPlugin } from "@vuepress/plugin-prismjs";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -13,7 +16,19 @@ export default defineUserConfig({
   description: "这是首页",
   
   head:[
-
+    [ // 百度统计
+      'script',
+      {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?95102fe909d0feeeff8713ebccb5f678";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+    ]
   ],
   theme,
   plugins: [
@@ -28,6 +43,44 @@ export default defineUserConfig({
     //   // 自定义热键
     //   hotKeys: [{ key: 'k', ctrl: true }],
     // }),
+    //评论
+    commentPlugin({
+      //选择哪款插件
+      provider: "Giscus",
+      comment: true,
+      //Giscus仓库地址 直接开启的本仓库的评论服务
+      repo:"zhaoxin1998/debugKing-github.io",
+      repoId:"R_kgDOJ7W1KQ",
+      category:"Announcements",
+      categoryId:"DIC_kwDOJ7W1Kc4CX9R5",
+      mapping:"pathname",
+      strict:true,
+    }),
+    //复制版权
+    copyrightPlugin({
+      author: "Mr.debugKing",
+      license: "MIT",
+      canonical: "https://zhaoxin1998.github.io/debugKing-github.io/",
+      global: true,
+      triggerWords: 5
+    }),
+    //图片复制下载
+    lightgalleryPlugin({
+      //操作页面 DOM 的延时，单位 ms
+      delay:1000
+    }),
+    //预计阅读时间与字数统计生成
+    readingTimePlugin({}),
+    //全文检索
+    searchProPlugin({
+      indexContent: true,
+      hotReload: true,
+    }),
+    //进度条
+    nprogressPlugin(),
+    //代码高亮
+    prismjsPlugin({
+      preloadLanguages:['markdown', 'jsdoc', 'yaml']
+    })
   ],
-
 });
